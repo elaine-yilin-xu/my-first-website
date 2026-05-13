590508869ae8ba956f71acb271591ab846ecc68f
elaine-yilin-xu <elainex@student.unimelb.edu.au>
2026-05-13 19:57:00 +1000

    added wishlist icon to product, and metadata (volume, skintype, breif description to product cards, and fine-tuned css layout

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const products = [
         price: "A$90.00",
         image: "images/product1.jpg",
         link: "revital-eyes-firming-gel.html",
+        skin: "Recommended for all skin type",
+        description: "A unique formulation of Eyebright Extract and Active Caffeine Complex awakens the skin.",
+        volume: "30 mL",
         tags: []
     },
 
@@ -13,6 +16,9 @@ const products = [
         price: "A$45.00",
         image: "images/product2.jpg",
         link: "age-defying-hand-cream.html",
+        skin: "Recommended for dry, aging skin",
+        description: "Silky-soft and quick absorbing, this cream has harnessed the power of Oleuropein.",
+        volume: "120 mL",
         tags: []
     },
 
@@ -21,6 +27,9 @@ const products = [
         price: "A$60.00",
         image: "images/product3.jpg",
         link: "desensitising-elixir.html",
+        skin: "Recommended for all skin type",
+        description: "A soothing blend with Lavender and Chamomile Essential Oil to calm skin.",
+        volume: "30 mL",
         tags: []
     },
 
@@ -29,6 +38,9 @@ const products = [
         price: "A$60.00",
         image: "images/product4.jpg",
         link: "eye-contour-elixir.html",
+        skin: "Recommended for all skin type",
+        description: "A superior blend rich in Omega 3 and 6, restores moisture and smoothness around eye.",
+        volume: "30 mL",
         tags: []
     },
 
@@ -37,6 +49,9 @@ const products = [
         price: "A$75.00",
         image: "images/product5.jpg",
         link: "hydrating-mud-mask.html",
+        skin: "Recommended for all skin types",
+        description: "An active clay designed to cleanse and hydrate dry and normal skin types.",
+        volume: "90 mL",
         tags: []
     },
 
@@ -45,6 +60,9 @@ const products = [
         price: "A$35.00",
         image: "images/product6.jpg",
         link: "hydrating-conditioner.html",
+        skin: "Recommended for dry, dehydrated skin",
+        description: "A nourishing conditioner with Neem Seed Oil and Wheat Protein to assist in hydrating hair.",
+        volume: "250 mL",
         tags: []
     },
 
@@ -53,6 +71,9 @@ const products = [
         price: "A$75.00",
         image: "images/product7.jpg",
         link: "age-defying-elixir.html",
+        skin: "Recommended for dry, ageing skin",
+        description: "Rejuvenated with luxurious Rose hip, Evening Primrose Oils, Sandalwood & Fennel Essential Oils.",
+        volume: "30 mL",
         tags: ["aging", "elixir-serum"]
     },
 
@@ -61,33 +82,58 @@ const products = [
         price: "A$40.00",
         image: "images/product8.jpg",
         link: "nourishing-dry-body-oil.html",
+        skin: "Recommended for all skin types",
+        description: "An exotic tropical blend of Jasmine, Geranium and Patchouli essential oils.",
+        volume: "125 mL",
         tags: []
     }
 
  
 ];
 
-const productsContainer = document.getElementById("products-container");
+const productsContainer = document.getElementById("products-container"); // find html with id = products-container
 
+// take list of products and display them on the page
 function renderProducts(productList) {
-    productsContainer.innerHTML = "";
+    productsContainer.innerHTML = ""; // wipes container clean
+    // dynamic values and adding html dynamically
     productList.forEach(product => {
-        productsContainer.innerHTML += `
+        productsContainer.innerHTML += ` 
             <article class="product-card">
-                <a href="${product.link}">
+                <a href="${product.link}"> 
                     <img src="${product.image}" alt="${product.name}">
-                    <h3>${product.name}</h3>
-
                 </a>
-                <div class="product-bottom">
-                    <p>${product.price}</p>
-                    <button type="button">ADD TO CART</button>
+                <div class="product-info">
+                    <div class="product-title-row">
+                        <h3>${product.name}</h3>
+                        <img 
+                            src="images/heart.png"
+                            alt="Wishlist"
+                            class="heart-icon"
+                        >
+                    </div>
+                    <p class="product-skin">
+                        ${product.skin}
+                    </p>
+                    <p class="product-description">
+                        ${product.description}
+                    </p>
+                    <p class="product-volume">
+                        ${product.volume}
+                    </p>
+                    <div class="product-bottom">
+                        <p class="product-price-card">${product.price}</p>
+                        <button type="button">
+                            ADD TO CART
+                        </button> 
+                    </div>
                 </div>
             </article>
         `;
     });
 }
 
+// if the product section exist, display them
 if (productsContainer) {
     renderProducts(products);
 }
@@ -95,6 +141,7 @@ if (productsContainer) {
 const applyBtn = document.querySelector(".apply-btn");
 const resetBtn = document.querySelector(".reset-btn");
 
+// && means logical AND 
 if (applyBtn && productsContainer) {
     applyBtn.addEventListener("click", () => {
         const checkedFilters = Array.from(
